555abd220bcf9402c0cdf7d3cf8690c28a70528c
Ben Morgan-Cleveland <benmorgancleveland@gmail.com>
2020-09-19 16:13:05 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,14 +1,16 @@
 array_of_arrays = [ [10, 11], [99, 50, 3, 4], [23, 41] ]
 
-def find_even_values(array_of_arrays)
+def find_even_values(src)
   # Given an Array of Arrays of Integers ( [ [10, 11], [99, 50, 3, 4], [23, 41] ] ):
   #
   # Output all even values in each nested array
     row_index = 0 
-    while row_index < array_of_arrays.count do
+    while row_index < src.count do
       element_index = 0 
-      while element_index < array_of_arrays[row_index].count do
-        p array_of_arrays[row_index][element_index] % 2
+      while element_index < src[row_index].count do
+        if src[row_index][element_index].even?
+          p src[row_index][element_index]
+        end
         element_index += 1 
       end
       row_index += 1
